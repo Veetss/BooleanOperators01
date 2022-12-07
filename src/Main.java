52b8303f453dce71579c5d4759bb9b69915b69e8
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:58:19 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+//PROVA "CARTACEA"
+//[A]: 2 <= 2 && !true = vero e non vero(falso) -> F
+//[B]: !false && 3 > 2 = non falso(vero) e vero -> T
+//[C]: considering that t=false and f=true: !(!t || f) = falso || falso -> F
+//[D]: 6 > 6 ^ !(true && true) = falso ^ falso -> F
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean t = false;
+        boolean f = true;
+
+        System.out.println(2 <= 2 && !true);
+        System.out.println(!false && 3 > 2 );
+        System.out.println(!(!t || f));
+        System.out.println(6 > 6 ^ !(true && true));
     }
 }
